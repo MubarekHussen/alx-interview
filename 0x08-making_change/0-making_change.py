b30997cf5fddfc6e506e6681f1315b641ba4c68e
MubarekHussen <mubahussen2014@gmail.com>
2023-02-27 11:30:56 +0300

    checking

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+""" Change comes from within
+"""
 
 
-def make_change(coins, total):
-    """Determines the fewest number of coins needed \
-        to meet a given amount total"""
-    if total <= 0:
-        return 0
-
-    current_total = 0
-    used_coins = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        r = (total - current_total) // coin
-        current_total += r * coin
-        used_coins += r
-        if current_total == total:
-            return used_coins
-    return -1
+def makeChange(coins, total):
+    """ Return the fewest number of coins needed to meet total
+    """
+    mx = total + 1
+    dp = [0] + [mx for _ in range(total)]
+    for i in range(1, len(dp)):
+        for c in coins:
+            if i - c >= 0:
+                dp[i] = min(dp[i], dp[i - c] + 1)
+    return dp[-1] if dp[-1] != total + 1 else - 1
